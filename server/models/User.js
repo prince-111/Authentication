566aa3ae28359c57dcf601a11d10268b4dfacff5
Prince Kumar <princesd991@gmail.com>
2024-06-29 22:40:38 +0530

    add userSchema

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "superAdmin"],
+    default: "user",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  resetPasswordToken: {
+    type: String,
+    default: "",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
