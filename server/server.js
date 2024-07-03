b0d8a506d70069931b0dec708bc934dbfbfeca1f
Prince Kumar <princesd991@gmail.com>
2024-07-03 19:24:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpecs = require("./swagger");
 const noteRoutes = require("./routes/noteRoutes");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -20,18 +23,23 @@ app.get('/', (req, res)=>{
     res.send("Hello From the Authentication Server!");
 })
 
+// Swagger documentation route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
+
+app.use("/api/v1", authRoutes)
 app.use("/api/v1/notes", noteRoutes);
-
+app.use("/api/v1/users", userRoutes);
 
 // Mongo Connection
 const URI = process.env.MONGODB_URI;
 mongoose
   .connect(URI, {
-    useNewUrlParser: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.error("MongoDb Connection Failed",err));
+  .catch(err => console.error("MongoDb Connection Failed", err));
 
 
 mongoose.connection.once("error",(err)=>{
