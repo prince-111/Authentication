5d903afa6e408a326fdd84e9c8ebed770a92cf19
Prince Kumar <princesd991@gmail.com>
2024-07-07 13:54:58 +0530

    🏃‍♂️ coming soon add in fronted

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-[#242626] w-1/2 h-1/2 border-2 rounded-3xl grid place-content-center">
+        <p>
+         <span className="text-green-500 font-semibold">Stellar Notes: </span> <br /> Capture Your Thoughts, Secure Your Data, <br /> Your Notes
+         safe, Authenticated and Protected.
+        </p>
+        <h1 className="text-orange-400">Coming Soon......</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home
