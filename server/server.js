b226a49baa1efd8c1760d30863110016701ee18e
Prince Kumar <princesd991@gmail.com>
2024-07-08 22:43:27 +0530

    🍪 use session and cookieParser

--- server/server.js
+++ server/server.js
@@ -7,12 +7,29 @@ const swaggerSpecs = require("./swagger");
 const noteRoutes = require("./routes/noteRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 
 const app = express();
 
 
 // middleware
 app.use(cors());
+app.use(express.json());
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookies: { maxAge: 3600000 } // 1 hour
+}));
+
+// Middleware to pase JSON Bodies
 app.use(express.json());
 
 // Define PORT
@@ -47,3 +64,7 @@ mongoose.connection.once("error",(err)=>{
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+console.log("Swagger documentation url:- ","http://localhost:5000/api-docs/");
+
+
+
