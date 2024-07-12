1af9fe12a11351116b85a6d51ce9180ec96d355d
Prince Kumar <princesd991@gmail.com>
2024-07-12 23:20:28 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,12 @@
-const User = require("../models/User");
+  const User = require("../models/User");
 
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+  exports.getUsers = async (req, res) => {
+    try {
+      // .select is for hide password from json object
+      const users = await User.find().select('-password');
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
