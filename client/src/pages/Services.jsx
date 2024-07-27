92eb8130fe8e9e278d998a3a6ef8ba45e09e949b
Prince Kumar <princesd991@gmail.com>
2024-07-27 23:48:06 +0530

    add the what implement next

---
+++ client/src/pages/Services.jsx
@@ -0,0 +1,30 @@
+
+
+const Services = () => {
+  return (
+   <>
+   <div className="grid place-content-center w-full h-screen">
+    Our Service
+    
+    <div>
+    future plan:- 
+    </div>
+    <div>
+        chat with ai
+    </div>
+    <div>
+        payment integration
+    </div>
+    <div>
+        geofancing
+    </div>
+    <div>
+        calender schedule meeting
+    </div>
+    <div>and 💡💡💡💡</div>
+   </div>
+   </>
+  )
+}
+
+export default Services
