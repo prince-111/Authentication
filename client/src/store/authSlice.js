6a0048ded64f378e684e6403ab1979a7a82629eb
Prince Kumar <princesd991@gmail.com>
2024-08-27 23:57:40 +0530

    create auth slice

---
+++ client/src/store/authSlice.js
@@ -0,0 +1,76 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import * as authApi from "../services/AuthApi";
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await authApi.register(userData);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (credentials, { rejectWithValue }) => {
+    try {
+       console.log("Login thunk called with:", credentials);
+      const response = await authApi.login(credentials);
+      console.log("API Response:", response);
+      return response.data;
+    } catch (error) {
+       console.log("Login thunk called");
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    token: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    logout: state => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(register.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(login.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        state.token = action.payload.token;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
